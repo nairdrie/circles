47513cfa17ea4f2976e2f26361d184a09a06ac83
Nick Airdrie <nick.airdrie@gmail.com>
2023-04-26 00:12:33 -0400

    add working auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,15 +5,16 @@ import { SessionProvider } from "next-auth/react";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
   return (
-    <SessionProvider session={session}>
+    <ClerkProvider {...pageProps} >
       <Component {...pageProps} />
-    </SessionProvider>
+    </ClerkProvider>
   );
 };
 
