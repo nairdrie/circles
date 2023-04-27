98f4c7d0253d5782b97ebd7df356621aded28571
Nick Airdrie <nick.airdrie@gmail.com>
2023-04-27 02:33:07 -0400

    remove async

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -31,7 +31,7 @@ type CreateContextOptions = {
  *
  * @see https://trpc.io/docs/context
  */
-export const createTRPCContext = async (opts: CreateNextContextOptions) => {
+export const createTRPCContext = (opts: CreateNextContextOptions) => {
   const { req, res } = opts;
 
   // Get the session from the server using the getServerSession wrapper function
