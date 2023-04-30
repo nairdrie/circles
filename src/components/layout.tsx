4be54420eb74337d1f2bcda4941e6cea15514291
Nick Airdrie <nick.airdrie@gmail.com>
2023-04-30 18:46:12 -0400

    github CI

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,6 @@
 import type { PropsWithChildren } from "react";
 
-export const PageLayout = (props: PropsWithChildren<{}>) => {
+export const PageLayout = (props: PropsWithChildren) => {
     return (
         <main className="flex justify-center h-screen">
             <div className="w-full h-full md:max-w-2xl border-x border-slate-400 overflow-y-scroll">
